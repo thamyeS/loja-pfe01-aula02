daaa26ac10d9646e85b38a723b5bb9e87b0a14df
thamyeS <sthamyecorrea@gmail.com>
2025-03-19 19:33:39 -0300

    refeito

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('oi')
+const uri = './assets/dados.json';
+var produtos = [];
+
+//Carrega os dados do Mockup
+fetch(uri)
+    .then(resp => resp.json())
+    .then(resp => { produtos = resp; })
+    .then(() => mostrarCards())
+
+function mostrarCards() {
+    const main = document.querySelector('main');
+    produtos.forEach(p => {
+        main.innerHTML += `
+        <div class="card">
+            <h3>${p.produto}</h3>
+            <img src="${p.imagem}" alt="${p.produto}">
+            <p>Preco: ${p.preco}</p>
+            <button>Detalhes</button>
+        </div>
+        `;
+    })
+}
